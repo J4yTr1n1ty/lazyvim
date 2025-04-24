03ba1bf2cde387ce1d6149ba5da2fe2016153d1e
Jay <jay@projectchaos.ch>
2025-04-24 08:56:09 +0200

    added colorcolumn 80

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,5 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.api.nvim_set_hl(0, "VisualNonText", { fg = "#5D5F71", bg = "#24282d" })
+
+vim.opt.colorcolumn = "80"
