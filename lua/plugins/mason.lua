4372f7c3fe2b8bc5d2e054a1cf34e2b1413b7886
Jay <jay@projectchaos.ch>
2025-11-24 19:04:20 +0100

    add extra mason registry

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,4 +1,10 @@
 return {
   "mason-org/mason.nvim",
-  opts = { ensure_installed = { "templ" } },
+  opts = {
+    ensure_installed = { "templ" },
+    registries = {
+      "github:mason-org/mason-registry",
+      "github:Crashdummyy/mason-registry",
+    },
+  },
 }
